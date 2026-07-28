fb6df4f009bc913604b60438819758e742e1d4cb
moritz-grimm <molumo@icloud.com>
2026-07-29 00:09:47 +0200

    move impressum and privacy policy links into static html; label main nav

--- src/scripts/footer.ts
+++ src/scripts/footer.ts
@@ -1,30 +1,25 @@
-type LastUpdatedResponse = {
-    lastUpdated: string,
-};
-
-const footerEl = document.getElementById("footer-text");
-
 // Copyright
-if (footerEl) {
-    footerEl.innerHTML = `Copyright © 2025 - ${new Date().getFullYear()} 
-                            | moritz-grimm.dev 
-                            | Built by Moritz 
-                            | <a href="/impressum.html">Impressum</a>
-                            | <a href="/privacy-policy.html">Privacy Policy</a>
-                            | <a href="https://status.moritz-grimm.dev/status/default" target="_blank" rel="noopener noreferrer" data-umami-event="footer-status-click">Status</a>`;
-}
-
-// Last Updated
-const lastUpdatedEl = document.getElementById("last-updated");
-if (lastUpdatedEl) {
-    const res = await fetch("https://api.moritz-grimm.dev/last-updated/moritz-grimm.dev");
-    const date = await res.json() as LastUpdatedResponse;
-    lastUpdatedEl.textContent = `Last Updated: ${date.lastUpdated ?? "Unknown"}`;
+const footerYear = document.getElementById("footer-year");
+if (footerYear) {
+    footerYear.textContent = `${new Date().getFullYear()}`;
 }
 
 // Version
 const versionEl = document.querySelectorAll(".app-version");
-
 versionEl.forEach(element => {
     element.textContent = `v${__APP_VERSION__ ?? "v1.0.0"}`;
 });
+
+// Last Updated
+const lastUpdatedEl = document.getElementById("last-updated");
+if (lastUpdatedEl) {
+    let date;
+    try {
+        const res = await fetch("https://api.moritz-grimm.dev/last-updated/moritz-grimm.dev");
+        date = await res.json() as { lastUpdated?: string };
+    } catch (err) {
+        console.error("Error fetching from api.moritz-grimm.dev", err);
+    }
+
+    lastUpdatedEl.textContent = `Last Updated: ${date?.lastUpdated ?? "Unknown"}`;
+}
